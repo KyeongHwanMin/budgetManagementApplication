20e8dbc422ab5abae143a9233debda0a47614523
KyeongHwan Min <h45652@naver.com>
2023-11-13 13:22:06 +0900

    FEAT : budget 앱 생성 및 등록

--- account/serializers.py
+++ account/serializers.py
@@ -26,6 +26,6 @@ class SignupSerializer(serializers.ModelSerializer):
         return user
 
 
-class LoginSerializer(serializers.Serialzier):
+class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
