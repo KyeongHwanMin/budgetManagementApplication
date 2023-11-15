84e198c5b96a3fb77885f63fc89a5228565ed508
KyeongHwan Min <h45652@naver.com>
2023-11-15 15:19:12 +0900

    FEAT : 오늘 지출 안내

--- expenditure/urls.py
+++ expenditure/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from expenditure.views import ExpenditureListView
+from expenditure.views import ExpenditureListView, DailyExpenditureView
 
 urlpatterns = [
     path("", ExpenditureListView.as_view()),
+    path("spending", DailyExpenditureView.as_view()),
 ]
--- expenditure/views.py
+++ expenditure/views.py
@@ -1,10 +1,12 @@
 from django.db.models import Sum, Q
+from django.utils import timezone
 from django.utils.dateparse import parse_date
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
+from budget.models import Budget
 from expenditure.models import Expenditure
 from expenditure.serializers import ExpenditureSerializer
 
@@ -35,7 +37,7 @@ class ExpenditureListView(APIView):
         if category:
             filters &= Q(category__name=category)
 
-        expenditures = Expenditure.objects.filter(filters)
+        expenditures = Expenditure.objects.filter(filters, excluded_total=False)
         total_expenditure = expenditures.aggregate(Sum("amount"))
         category_totals = expenditures.values("category__name").annotate(
             total=Sum("amount")
@@ -55,3 +57,52 @@ class ExpenditureListView(APIView):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DailyExpenditureView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        today = timezone.now().date()
+        today_expenditures = Expenditure.objects.filter(
+            user=request.user, date__date=today
+        )
+        month_budgets = Budget.objects.filter(
+            user=request.user, period__month=today.month, period__year=today.year
+        )
+
+        total_expenditure = (
+            today_expenditures.aggregate(total=Sum("amount"))["total"] or 0
+        )
+        category_totals = today_expenditures.values("category__name").annotate(
+            total=Sum("amount")
+        )
+
+        category_total_dict = {
+            item["category__name"]: item["total"] for item in category_totals
+        }
+
+        statistics = []
+        for budget in month_budgets:
+            category_name = budget.category.name
+            expected_amount = budget.amount / today.day
+            spent_amount = category_total_dict.get(category_name, 0)
+            print(category_name, spent_amount)
+            risk = (spent_amount / expected_amount) * 100 if expected_amount > 0 else 0
+
+            statistics.append(
+                {
+                    "category": category_name,
+                    "expected_amount": expected_amount,
+                    "spent_amount": spent_amount,
+                    "risk_percentage": risk,
+                }
+            )
+
+        return Response(
+            {
+                "total_expenditure": total_expenditure,
+                "category_totals": list(category_totals),
+                "statistics": statistics,
+            }
+        )
